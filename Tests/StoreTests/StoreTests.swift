a6852da97d30c501ca463300ae6408f74c4fb96b
baegteun <baegteun@gmail.com>
2023-05-18 01:02:34 +0900

    :white_check_mark: :: TestStore test

--- Tests/StoreTests/StoreTests.swift
+++ Tests/StoreTests/StoreTests.swift
@@ -1,8 +1,66 @@
+import Combine
 import XCTest
 @testable import Store
 
+final class TestStore: Store {
+    struct State: Equatable {
+        var count = 0
+    }
+
+    enum Action {
+        case increase
+    }
+
+    enum Mutation {
+        case increase
+    }
+
+    var initialState: State
+    var stateSubject: CurrentValueSubject<State, Never>
+    var subscription: Set<AnyCancellable> = .init()
+
+    init() {
+        self.initialState = .init()
+        self.stateSubject = CurrentValueSubject(initialState)
+    }
+
+    func mutate(state: State, action: Action) -> SideEffect<Mutation, Never> {
+        switch action {
+        case .increase:
+            return .just(.increase)
+        }
+    }
+
+    func reduce(state: State, mutate: Mutation) -> State {
+        var newState = state
+
+        switch mutate {
+        case .increase:
+            newState.count += 1
+        }
+
+        return newState
+    }
+}
+
 final class StoreTests: XCTestCase {
-    func testExample() throws {
-        XCTAssertEqual(1, 1)
+    var subscription = Set<AnyCancellable>()
+
+    func testStore() {
+        let store = TestStore()
+
+        let expectation = XCTestExpectation(description: "State Updated")
+
+        store.state
+            .dropFirst()
+            .sink { state in
+                XCTAssertEqual(state.count, 1)
+                expectation.fulfill()
+            }
+            .store(in: &subscription)
+
+        store.send(.increase)
+
+        wait(for: [expectation], timeout: 1.0)
     }
 }
